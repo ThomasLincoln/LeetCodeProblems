5a5150c123650997e0753e0de0bfa7c54cad622c
Thomas Lincoln <thmas5952@gmail.com>
2024-02-21 11:07:58 -0300

    feat: :sparkles: Add the solution of Reverse Vowels of a String solution made in JS

--- LeetCode 75/ReverseVowelsOfAString.js
+++ LeetCode 75/ReverseVowelsOfAString.js
@@ -7,59 +7,90 @@ Input: s = "leetcode";
 Output: "leotcede";
 
 function isVowel(x) {
-  var result;
-
-  result = x == "a" || x == "e" || x == "i" || x == "o" || x == "u";
+  let result;
+  result =
+    x == "a" ||
+    x == "e" ||
+    x == "i" ||
+    x == "o" ||
+    x == "u" ||
+    x == "A" ||
+    x == "E" ||
+    x == "I" ||
+    x == "O" ||
+    x == "U";
   return result;
 }
 
-function swap(x, y, string) {
-  let newString = "";
-  for (let i = 0; i < string.length; i++) {
-    newString += i === x ? string[y] : i === y ? string[x] : string[i];
-  }
-
-  return newString;
-}
-
-function findFirstVowel(s, i, j) {
-  while (i < j) {
-    if (isVowel(s[i])) {
-      return i;
-    }
-    i++;
-  }
-}
-
-function findLastVowel(s, i, j) {
-  while (j > i) {
-    if (isVowel(s[j])) {
-      return j;
-    }
-    j--;
-  }
+function swap(x, y, str) {
+  let a = str[x];
+  let b = str[y];
+  let aux = a;
+  str[x] = b;
+  str[y] = aux;
+  return str;
 }
 
 var reverseVowels = function (s) {
-  let newString = s;
-  const size = s.length;
-  let i = 0,
-  j = size - 1;
-  
-  while (i < j) {
-    i = findFirstVowel(newString, i, j);
-    // console.log(`First Vowel ${s[i]}`);
+  let newString = s.split(""),
+    auxString = s,
+    numberOfSwaps,
+    i = 0,
+    j = s.length,
+    firstVowel,
+    secondVowel,
+    numberOfVowels;
 
-    j = findLastVowel(newString, i, j);
-    // console.log(`Last Vowel ${s[j]}`)
+  // Get the number of vowels if have one
+  if (s.match(/[aeiou]/gi) != null) {
+    numberOfVowels = s.match(/[aeiou]/gi).length;
+  } else {
+    return s;
+  }
 
-    newString = swap(i, j, newString);
-    console.log(newString);
-    i--;
+  // Get the number of swaps
+  if (numberOfVowels % 2 == 0) {
+    numberOfSwaps = numberOfVowels / 2;
+  } else {
+    numberOfSwaps = parseInt((numberOfVowels - 1) / 2);
+  }
+
+  // Making swaps if it is needed
+  console.log(numberOfSwaps);
+  while (numberOfSwaps != 0) {
+    firstVowel = false;
+    secondVowel = false;
+    // console.log("Indices: "+ i + j)
+    while (i < j && firstVowel == false) {
+      firstVowel = isVowel(newString[i]);
+      if (!firstVowel) {
+        i++;
+      }
+    }
+    if (firstVowel) {
+      while (j >= i && secondVowel == false) {
+        secondVowel = isVowel(newString[j]);
+        if (!secondVowel) {
+          j--;
+        }
+      }
+    }
+    // console.log("I find the first vowel " + newString[i]);
+    // console.log("I find the second vowel " + newString[j])
+    // console.log(`I will change ${newString[i]} and ${newString[j]}`)
+    if (firstVowel && secondVowel) {
+      newString = swap(i, j, newString);
+    }
+    // console.log("I made a swap look at it: " + auxString)
+    numberOfSwaps--;
+    i++;
     j--;
   }
-  
-  return newString;
+  auxString = newString.join("");
+  return auxString;
 };
 
-console.log(reverseVowels("leetcode"));
+// const test = "A new order began, a more Roman age bred Rowena.";
+// const resposta = "a new order began, a more Roman age bred RowenA."
+// console.log("input: " + test);
+// console.log("output: " + reverseVowels(test) + resposta);
